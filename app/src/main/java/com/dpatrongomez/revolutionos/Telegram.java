69128c926d09b8eccf45224e88f20ba4bab348a2
Daniel Patrón Gómez <dpatrongomez@gmail.com>
2019-01-18 11:30:29 +0100

    Update Button links

--- app/src/main/java/com/dpatrongomez/revolutionos/Telegram.java
+++ app/src/main/java/com/dpatrongomez/revolutionos/Telegram.java
@@ -1,6 +1,8 @@
 package com.dpatrongomez.revolutionos;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +18,11 @@ public class Telegram extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_telegram);
         ImageButton back = findViewById(R.id.back);
+        Button bttelegram1 = findViewById(R.id.bttelegram1);
+        Button bttelegram2 = findViewById(R.id.bttelegram2);
+        Button bttelegram3 = findViewById(R.id.bttelegram3);
+        Button bttelegram4 = findViewById(R.id.bttelegram4);
+        Button bttelegram5 = findViewById(R.id.bttelegram5);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +30,43 @@ public class Telegram extends AppCompatActivity {
                 finish();
             }
         });
+        bttelegram1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/RevolutionMIUI"));
+                startActivity(browserIntent);
+            }
+        });
+        bttelegram2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://t.me/RevolutionNews"));
+                startActivity(browserIntent);
+            }
+        });
+        bttelegram3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://t.me/ROSDownloads"));
+                startActivity(browserIntent);
+            }
+        });
+        bttelegram4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/RevolutionAOSP"));
+                startActivity(browserIntent);
+            }
+        });
+        bttelegram5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/RevolutionMIUIen"));
+                startActivity(browserIntent);
+            }
+        });
+
+
     }
 }
 
