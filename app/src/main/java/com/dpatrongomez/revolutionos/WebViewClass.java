9f6eebeb1c9d74824a59860c527d9cfe4b934446
Daniel Patrón Gómez <dpatrongomez@gmail.com>
2019-02-02 12:59:30 +0100

    Update names and try download

--- app/src/main/java/com/dpatrongomez/revolutionos/WebViewClass.java
+++ app/src/main/java/com/dpatrongomez/revolutionos/WebViewClass.java
@@ -1,13 +1,24 @@
 package com.dpatrongomez.revolutionos;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.DownloadManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
 import android.webkit.WebSettings;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class WebViewClass extends AppCompatActivity {
 
@@ -18,7 +29,7 @@ public class WebViewClass extends AppCompatActivity {
         setContentView(R.layout.activity_webview);
         Intent intent = getIntent();
         String numero = intent.getStringExtra(MainActivity.numero);
-        String urlweb = "https://os.revtechs.me/";;
+        String urlweb = "https://os.revtechs.me/";
         int n = Integer.parseInt(numero);
 
         switch (n) {
@@ -38,17 +49,54 @@ public class WebViewClass extends AppCompatActivity {
 
                 break;
         }
-        android.webkit.WebView faq = findViewById(R.id.web);
-        faq.loadUrl(urlweb);
-        faq.getSettings().setJavaScriptEnabled(true);
-        faq.getSettings().setSupportZoom(true);
-        faq.getSettings().setUseWideViewPort(true);
-        faq.getSettings().setAllowContentAccess(true);
-        faq.getSettings().setAppCacheEnabled(true);
-        faq.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
-        faq.getSettings().setDomStorageEnabled(true);
-        faq.getSettings().setAllowFileAccess(true);
-        faq.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-        faq.setWebViewClient(new WebViewClient());
+        android.webkit.WebView web = findViewById(R.id.web);
+        web.loadUrl(urlweb);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.getSettings().setSupportZoom(true);
+        web.getSettings().setUseWideViewPort(true);
+        web.getSettings().setAllowContentAccess(true);
+        web.getSettings().setAppCacheEnabled(true);
+        web.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+        web.getSettings().setDomStorageEnabled(true);
+        web.getSettings().setAllowFileAccess(true);
+        web.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        web.setWebViewClient(new WebViewClient());
+
+
+        web.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimeType, long contentLength) {
+
+                if(!check_permission(1)){
+                    ActivityCompat.requestPermissions(WebViewClass.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},2);
+                }else {
+                    DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+
+                    request.setMimeType(mimeType);
+                    String cookies = CookieManager.getInstance().getCookie(url);
+                    request.addRequestHeader("cookie", cookies);
+                    request.addRequestHeader("User-Agent", userAgent);
+                    request.setDescription(getString(R.string.dl_downloading));
+                    request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType));
+                    request.allowScanningByMediaScanner();
+                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimeType));
+                    DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                    assert dm != null;
+                    dm.enqueue(request);
+                    Toast.makeText(getApplicationContext(), getString(R.string.dl_downloading2), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+    }
+    public boolean check_permission(int permission){
+        switch(permission){
+            case 1:
+                return ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+
+        }
+        return false;
     }
 }
